c98a7500e89bd80db22613103819a527e81658eb
Mykola Serdiuk <kolyaaxe@gmail.com>
2025-05-15 09:02:35 +0300

    Add source code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+_ = Task.Run(async () =>
+{
+    while (await timer.WaitForNextTickAsync())
+    {
+        Console.WriteLine("Starting CPU-intensive task");
+        var start = DateTime.UtcNow;
+        while ((DateTime.UtcNow - start).TotalMilliseconds < 500)
+        {
+            // Busy-wait loop
+            double _ = Math.Sqrt(start.Millisecond);
+        }
+        Console.WriteLine("Completed CPU-intensive task");
+    }
+});
 
+app.MapGet("/", () => "Hello from .NET CPU app!");
 app.Run();
